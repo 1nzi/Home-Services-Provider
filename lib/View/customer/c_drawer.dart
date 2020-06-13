157888e80035a6987cc09336dccc666ac7dfed80
Saad Malik <msaad5746@gmail.com>
2020-06-14 01:18:36 +0500

    worker pending task

--- lib/View/customer/c_drawer.dart
+++ lib/View/customer/c_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:home_well/Model/CustomerModel/customerProfileModel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:home_well/Controller/CustomerController/rigesterCustomerCtrl.dart';
+import 'package:home_well/View/common/AboutUs.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'c_history.dart';
@@ -151,7 +152,7 @@ class _MyDrawerPageState extends State<CustomerDrawerOnly> {
                 Navigator.push(
                     context,
                     new MaterialPageRoute(
-                        builder: (context) => new CustomerLogin()));
+                        builder: (context) => new AboutUs()));
               },
             ),
             new ListTile(
